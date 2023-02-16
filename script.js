238f20fa9086b028dc20b3b4d907e1c695a48fe1
Arnaldo Bismarques <arnaldo.bismarques@hotmail.com>
2023-02-16 15:25:27 +0100

    changes

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Vehicles = /** @class */ (function () {
     function Vehicles(bName, type, price, image, color) {
         this.bName = bName;
@@ -7,10 +22,20 @@ var Vehicles = /** @class */ (function () {
         this.image = image;
     }
     Vehicles.prototype.printCarName = function () {
-        return "<div class=\"card\" style=\"width: 18rem;\">\n        <img src=\"".concat(this.image, "\" class=\"card-img-top\" alt=\"").concat(this.bName, "\">\n        <div class=\"card-body\">\n          <h5 class=\"card-title Model\">").concat(this.bName, "</h5>\n          <p class=\"fuel\">Type: ").concat(this.type, "</p>\n          <p class=\"year\">Available Colour: ").concat(this.color, "</p>\n          <p class=\"location\"> Price: ").concat(this.price, "\u20AC</p>\n          <a href=\"#\" class=\"btn btn-primary\">Buy</a>\n        </div>\n      </div>");
+        return "<div class=\"card\" style=\"width: 18rem;\">\n        <img src=\"".concat(this.image, "\" class=\"card-img-top\" alt=\"").concat(this.bName, "\">\n        <div class=\"card-body\">\n          <h5 class=\"card-title Model\">").concat(this.bName, "</h5>\n          <p class=\"model\">Type: ").concat(this.type, "</p>\n          <p class=\"year\">Available Colour: ").concat(this.color, "</p>\n          <p class=\"location\"> Price: ").concat(this.price, "\u20AC</p>\n          <p class=\"kilometers left\"> Kilometers: ").concat(this.price, "\u20AC</p>\n          <p class=\"numbers of seats\"> Seats: ").concat(this.price, "\u20AC</p>\n          <p class=\"fuel type\"> Fuel Type: ").concat(this.price, "\u20AC</p>\n          <p class=\"year of production\"> Produced in: ").concat(this.price, "\u20AC</p>\n          <a href=\"#\" class=\"btn btn-primary\">Buy</a>\n        </div>\n      </div>");
     };
     return Vehicles;
 }());
+var Motorbikes = /** @class */ (function (_super) {
+    __extends(Motorbikes, _super);
+    function Motorbikes(bName, type, price, image, color, motorbike, trucks) {
+        var _this = _super.call(this, bName, type, price, image, color) || this;
+        _this.motorbike = motorbike;
+        _this.trucks = trucks;
+        return _this;
+    }
+    return Motorbikes;
+}(Vehicles));
 var astonMartin = document.getElementById("carname");
 var astonMartindb7 = new Vehicles("Aston Martin", "sportscar", 50000, "https://cdn.pixabay.com/photo/2017/03/05/15/29/aston-martin-2118857__340.jpg", "red");
 astonMartin.innerHTML = astonMartindb7.printCarName();
@@ -23,3 +48,9 @@ Audi.innerHTML = AudiA7.printCarName();
 var VW = document.getElementById("carname4");
 var VWBeetle = new Vehicles("VW Beetle", "Coupe", 30000, "https://cdn.pixabay.com/photo/2017/10/03/17/26/vw-2813334__340.jpg", "yellow");
 VW.innerHTML = VWBeetle.printCarName();
+var Suzuki = document.getElementById("motorbike1");
+var Suzukisuperbike = new Vehicles("Suzuki", "Bike", 25000, "https://cdn.pixabay.com/photo/2019/03/15/09/09/bike-4056618__340.jpg", "blue");
+Suzuki.innerHTML = Suzukisuperbike.printCarName();
+var MAN = document.getElementById("Truck1");
+var Truck1 = new Vehicles("MAN", "Truck", 70000, "https://cdn.pixabay.com/photo/2017/08/17/05/59/peterbilt-2650184__340.jpg", "grey");
+MAN.innerHTML = Truck1.printCarName();
--- script.ts
+++ script.ts
@@ -16,13 +16,27 @@ class Vehicles{
         <img src="${this.image}" class="card-img-top" alt="${this.bName}">
         <div class="card-body">
           <h5 class="card-title Model">${this.bName}</h5>
-          <p class="fuel">Type: ${this.type}</p>
+          <p class="model">Type: ${this.type}</p>
           <p class="year">Available Colour: ${this.color}</p>
           <p class="location"> Price: ${this.price}€</p>
+          <p class="kilometers left"> Kilometers: ${this.price}€</p>
+          <p class="numbers of seats"> Seats: ${this.price}€</p>
+          <p class="fuel type"> Fuel Type: ${this.price}€</p>
+          <p class="year of production"> Produced in: ${this.price}€</p>
           <a href="#" class="btn btn-primary">Buy</a>
         </div>
       </div>`;
       }
+      }
+      class Motorbikes extends Vehicles{
+        motorbike: string;
+        trucks: string;
+
+        constructor (bName: string, type: string, price: number, image: string, color: string, motorbike: string, trucks: string) {
+            super(bName, type, price, image, color);
+            this.motorbike = motorbike;
+            this.trucks = trucks;
+        }
       }
 let astonMartin = document.getElementById("carname") as HTMLElement
 let astonMartindb7 = new Vehicles ("Aston Martin", "sportscar", 50000, "https://cdn.pixabay.com/photo/2017/03/05/15/29/aston-martin-2118857__340.jpg","red");
@@ -40,3 +54,10 @@ let VW = document.getElementById("carname4") as HTMLElement
 let VWBeetle = new Vehicles ("VW Beetle", "Coupe", 30000, "https://cdn.pixabay.com/photo/2017/10/03/17/26/vw-2813334__340.jpg", "yellow");
 VW.innerHTML = VWBeetle.printCarName();
 
+let Suzuki = document.getElementById("motorbike1") as HTMLElement
+let Suzukisuperbike = new Vehicles ("Suzuki", "Bike", 25000, "https://cdn.pixabay.com/photo/2019/03/15/09/09/bike-4056618__340.jpg", "blue");
+Suzuki.innerHTML = Suzukisuperbike.printCarName();
+
+let MAN = document.getElementById("Truck1") as HTMLElement
+let Truck1 = new Vehicles ("MAN", "Truck", 70000, "https://cdn.pixabay.com/photo/2017/08/17/05/59/peterbilt-2650184__340.jpg", "grey");
+MAN.innerHTML = Truck1.printCarName();
